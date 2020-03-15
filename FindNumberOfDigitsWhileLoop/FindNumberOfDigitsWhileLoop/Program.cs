1534d09c9f24065aa484b51542d676745a79173a
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 10:56:10 +0530

    Add files via upload

---
+++ FindNumberOfDigitsWhileLoop/FindNumberOfDigitsWhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FindNumberOfDigitsWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 15243; int count = 0; int digit = number;
+            while(number>0)
+            {
+                ++count;
+                number =  number/ 10;
+            }
+            Console.WriteLine("Given digit {0} contains {1} digits ", digit, count);
+            Console.ReadLine();
+        }
+    }
+}
